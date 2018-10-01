dc677a8eaa9566bbdbe186ce82ddbfe695bf4ccb
root <root@renex-vm.us-central1-f.c.renex-216414.internal>
2018-10-01 16:11:31 +0000

    Second Commit

--- sample-data.php
+++ sample-data.php
@@ -1,10 +1,34 @@
 <?php
 	class User{
-		function User( $u, $p, $a){
+		public $username = "default";
+		private $pin = "4789";
+		private $admin = false;	
+		
+		function User( $u, $p){
 			$this->username = $u;
 			$this->pin = $p;
+			$this->message = "Hello World";
+		}
+
+		function setPinAndAdmin($p, $a){
+			$this->pin = $p;
 			$this->admin = $a;
-		}	
+		}
+		
+		function setPin($p){
+			$this->pin = $p;
+		}
+		function setAdmin($a){
+			$this->admin = $a;
+		}
+		function getPin(){	
+			return $this->pin;
+		}
+
+		function getAdmin(){
+			return $this->admin;
+		}
+			
 	}
 	
 
